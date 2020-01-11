9d7dfc30ae1684c5579a1c4bbf2bbfeb70e3d05a
tomerperetz <tomerperetz@gmail.com>
2020-01-11 11:53:49 +0200

    ip address changed to tomer's pc

--- Shared/SocketExampleShared.h
+++ Shared/SocketExampleShared.h
@@ -12,7 +12,7 @@
 
 /*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
 
-#define SERVER_ADDRESS_STR "127.0.0.1"
+#define SERVER_ADDRESS_STR "192.168.1.105"
 #define SERVER_PORT 2345
 
 #define STRINGS_ARE_EQUAL( Str1, Str2 ) ( strcmp( (Str1), (Str2) ) == 0 )
